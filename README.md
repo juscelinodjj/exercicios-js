1695de378dc87ab51ace20d2789e14382452c044
Juscelino <juscelinodjj@gmail.com>
2021-03-02 19:22:44 -0300

    Adding palindromic number

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Exercícios JavaScript
 
 - [Número Primo](https://github.com/juscelinodjj/exercicios-js/blob/main/prime-number.js)
+- [Número Palíndromo](https://github.com/juscelinodjj/exercicios-js/blob/main/palindromic-number.js)
---
+++ palindromic-number.js
@@ -0,0 +1,25 @@
+const palindromicNumber = (min, max) => {
+  const isInvalidMin = min < 0;
+  const isInvalidMax = max < min || max === min;
+  if (isInvalidMin || isInvalidMax) {
+    return 'Insira dois números válidos, sendo o primeiro maior ou igual a zero'
+    + ' e o segundo maior que o primeiro.';
+  }
+  const range = Array((max - min) + 1)
+    .fill(null)
+    .map((element, index) => min + index);
+  const palindromes = [];
+  for (const number of range) {
+    const reverseNumber = parseInt(
+      String(number).split("").reverse().join(""));
+    const isPalindromic = reverseNumber === number;
+    if (isPalindromic) {
+      palindromes.push(number);
+    }
+  }
+  return palindromes.join('-');
+};
+
+const result = palindromicNumber(0, 100);
+console.log(result);
+// 0-1-2-3-4-5-6-7-8-9-11-22-33-44-55-66-77-88-99
