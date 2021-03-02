2dffd46fb161ba11b503d39e12da38a08c3199b6
Juscelino <juscelinodjj@gmail.com>
2021-03-02 19:00:10 -0300

    Adding prime number

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Exercícios JavaScript
+
+- [Número Primo](https://github.com/juscelinodjj/exercicios-js/blob/main/prime-number.js)
---
+++ prime-number.js
@@ -0,0 +1,25 @@
+const primeNumber = number => {
+  const isInvalidNumber = isNaN(number) || number < 2;
+  if (isInvalidNumber) {
+    return 'Insira um número válido, sendo ele maior ou igual a dois.';
+  }
+  const primes = [];
+  const numbers = Array(parseInt(number))
+    .fill(null)
+    .map((element, index) => ++index);
+  for (const number of numbers) {
+    const dividers = Array(number)
+      .fill(null)
+      .map((element, index) => ++index);
+    const divisibles = dividers.filter(divider => number % divider === 0);
+    const isPrime = divisibles.length === 2;
+    if (isPrime) {
+      primes.push(number);
+    }
+  }
+  return primes.join('-');
+};
+
+const result = primeNumber(100);
+console.log(result);
+// 2-3-5-7-11-13-17-19-23-29-31-37-41-43-47-53-59-61-67-71-73-79-83-89-97
